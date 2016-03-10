616c22389643a6090ddfbf27e3b350fe1be8b385
Matt Allan <matthew.james.allan@gmail.com>
2016-03-10 12:40:25 -0500

    correct the url in non web links

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
                 var url = 'https://asciinema.org/a/';
 
                 if (this.generator != "website") {
-                    return '<img src="' + url + castId + '.png" alt="Asciinema screencast thumbnail" /><p><a href="'+url+'">Screencast link</a></p>';
+                    return '<img src="' + url + castId + '.png" alt="Asciinema screencast thumbnail" /><p><a href="'+url+ castId + '">Screencast link</a></p>';
                 }
 
                 return '<div style="position: relative;padding-bottom: 56.25%;padding-top: 25px;height: 0;">'
